61d68d8c59fbf955e85739e059923772fdfa4c81
Jonathan Nuñez <37056901+JonathanNunezRdz@users.noreply.github.com>
2023-02-17 01:31:09 -0600

    detail (client): show skeleton on first load

--- apps/client/src/pages/media/waifus/index.tsx
+++ apps/client/src/pages/media/waifus/index.tsx
@@ -20,17 +20,25 @@ import {
 } from '@client/src/store/media';
 import Body from '@client/src/components/layout/Body';
 import WaifuCard from '../../waifus/WaifuCard';
+import MediaActionButtons from '../MediaActionButtons';
 
 const MediaWaifus = () => {
+	// redux hooks
 	const dispatch = useAppDispatch();
-	const router = useRouter();
-	const mediaId = router.query.mediaId;
 	const { isLoggedIn } = useAppSelector(selectAuth);
 	const { id: ownId } = useAppSelector(selectUser);
 	const { status } = useAppSelector(selectMediaWaifusStatus);
 	const { media } = useAppSelector(selectMediaWaifus);
 	const isLoaded = status === 'succeeded';
+	const isKnown =
+		isLoaded &&
+		media.knownBy.findIndex((user) => user.userId === ownId) > -1;
 
+	// next hooks
+	const router = useRouter();
+	const mediaId = router.query.mediaId;
+
+	// react hooks
 	useEffect(() => {
 		if (router.isReady) {
 			if (status === 'idle') {
@@ -55,20 +63,28 @@ const MediaWaifus = () => {
 			<VStack w='full' spacing='4'>
 				<Box w='full'>
 					<HStack spacing='4'>
-						<Box>
-							<Skeleton
-								isLoaded={isLoaded}
-								mb={isLoaded ? '0' : '2'}
-							>
-								<Heading>
-									{isLoaded ? media.title : 'Loading'}
-								</Heading>
-							</Skeleton>
-							<Skeleton isLoaded={isLoaded} fadeDuration={1}>
-								<Text>{isLoaded ? media.type : 'Loading'}</Text>
-							</Skeleton>
-						</Box>
+						<Skeleton isLoaded={isLoaded}>
+							<Heading>
+								{isLoaded ? media.title : 'loading'}
+							</Heading>
+						</Skeleton>
+						<Skeleton isLoaded={isLoaded}>
+							{isLoaded && (
+								<MediaActionButtons
+									isLoggedIn={isLoggedIn}
+									query={{
+										knownByMe: isKnown,
+										mediaIdString: media.id,
+										mediaTitle: media.title,
+										mediaTypeString: media.type,
+									}}
+								/>
+							)}
+						</Skeleton>
 					</HStack>
+					<Skeleton isLoaded={isLoaded} fadeDuration={1}>
+						<Text>{isLoaded ? media.type : 'loading'}</Text>
+					</Skeleton>
 				</Box>
 				<Box w='full'>
 					<SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing='4'>
